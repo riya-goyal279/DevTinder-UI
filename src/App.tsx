4b86fa14f443b422e906795b8192cc6cd4721d4f
Riya Goyal <riya.goyal@axeno.co>
2025-08-22 06:25:51 +0530

    added toggle theme dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-red-500">Hello World</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="text-red-500">Hello World</h1>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
+        <ModeToggle />
+      </ThemeProvider>
     </>
   )
 }
